d9e1b5e840c5bf08e060eb03fbac42dc389645e7
bryon burke <bryonburke@bryons-MBP.hsd1.or.comcast.net>
2019-12-13 09:33:34 -0800

    bunde install not recognized, troubleshooting

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -6,6 +6,23 @@ class Anagram
   end
 
   def test_palindrome()
+    word_one_array = []
+    word_two_array = []
+    word_one_array.push(word_one)
+    word_one_array.split
+    word_one_array.sort
+    word_one_array.push(word_two)
+    word_one_array.split
+    word_one_array.sort
+    if word_one_array = word_two_array
+      anagram = true
+    else
+      anagram = false
+    end
+
+
+
+    @result_array.push(number)
 
 
   end
--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -3,7 +3,7 @@ require('ruby_logic')
 
 describe('#ruby_logic') do
   it("tests to see if a word is a palindrome") do
-
-
-
+    my_word = Anagram.new(words, words)
+    expect(my_word.anagram).to(eq(true))
   end
+end
